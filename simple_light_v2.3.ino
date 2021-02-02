eebddbc2f9652cee7d4264b533fd8c0e4b8ca13a
cctucker5 <77966337+cctucker5@users.noreply.github.com>
2021-02-02 21:55:38 +0100

    adding the communication WORKING files
    simple_light_v2.3 is the code to run on Arduino (reads commands from ESP32 via serial as char)
    mqtt_esp32_PiBroker_Serial_v2.3 is the code to run on ESP32 (listens to broker and sends commands to Arduino and vis versa)
    talk_to_pi_v2 is the PC client base code (need to add capacity to make it coordinate bot actions)

---
+++ simple_light_v2.3.ino
@@ -0,0 +1,78 @@
+// 
+// Code to turn light on/off from recevied serial commands
+// 
+
+int redLED = 12;
+int bluLED = 8;
+
+
+int currentByte;  // variable to store incoming serial data
+String content;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(redLED, OUTPUT);
+  pinMode(bluLED, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available()>0){          // send data only if you receive data
+    currentByte = Serial.read();      // read the incoming byte
+
+    /*
+    // Testing Serial.read v. Serial.write
+    // read returns the byte sent as an int
+    // write returns the actual msg
+    Serial.println();
+    Serial.write(currentByte);
+
+    // read the serial data as a string, do something depending on what topic is present
+    content = Serial.readStringUntil('\n');
+    Serial.print(content);
+    if (content.indexOf("red") >= 0){
+      Serial.println();
+      Serial.println("OKAY");
+    }
+    */
+    
+    
+    //Serial.print("data received: ");
+    //Serial.println(currentByte);
+
+    if (currentByte == 49){           // keypress 1
+      on(redLED);
+      //Serial.println();
+      Serial.print("/red/on");
+    }
+
+    if (currentByte == 50){           // keypress 2
+      off(redLED);
+      //Serial.println();
+      Serial.print("/red/off");
+    }
+
+    if (currentByte == 51){           // keypress 3
+      on(bluLED);
+      //Serial.println();
+      Serial.print("/blue/on");
+    }
+    
+    if (currentByte == 52){           // keypress 3
+      off(bluLED);
+      //Serial.println();
+      Serial.print("/blue/off");
+    }
+  }
+}
+
+// My functions
+// Light On
+void on(int pinName){
+  digitalWrite(pinName, HIGH);
+}
+// Light Off
+void off(int pinName){
+  digitalWrite(pinName, LOW);
+}
+// Check for topic
+//void checkTopic(
